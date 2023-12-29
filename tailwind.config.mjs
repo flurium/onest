f6dc89f10544a8ba3ed779fd991e2e9f9a82cf87
Roman Koshchei <romankoshchei@gmail.com>
2023-12-29 19:46:52 +0200

    carousel

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Onest Variable", ...defaultTheme.fontFamily.sans],
+        sans: ["Onest Variable", "Onest", ...defaultTheme.fontFamily.sans],
       },
     },
   },
